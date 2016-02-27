9538507b661a956e295fa38696a9f3690c4bb6fe
Craig Carlson <cjcarlson20@gmail.com>
2016-02-26 20:59:59 -0500

    got heartbeat to read through unity; running into some issues, but it works

---
+++ HeartbeatHorror/Assets/Scripts/CMS50DPlus.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System;
+
+public class CMS50Dplus {
+
+	private string port;
+	private SerialPort conn;
+
+	public CMS50Dplus(string port) {
+		this.port = port;
+	}
+
+	public bool isConnected() {
+		return conn != null && conn.IsOpen;
+	}
+
+	void connect() {
+		if (conn == null) {
+			int baudrate = 19200;
+			conn = new SerialPort(port, baudrate, Parity.Odd, 8, StopBits.One);
+			conn.ReadTimeout = 5;
+			conn.Handshake = Handshake.XOnXOff;
+			conn.Open();
+		}
+		else if (!isConnected()) {
+			conn.Open();
+		}
+
+	}
+
+	void disconnect() {
+		if (isConnected()) {
+			conn.Close();
+		}
+	}
+
+	public IEnumerator getLiveData() {
+		connect();
+		while (true) {
+			byte[] packet = { 0, 0, 0, 0, 0 };
+			bool timeout = false;
+			try {
+				conn.Read(packet, 0, 5);
+			}catch (TimeoutException){
+				Debug.Log("timeout");
+				timeout = true;
+			}
+			if (timeout) {
+				yield return null;
+			}
+			else {
+				if (Convert.ToBoolean(packet[0] & 0x80)) {
+					yield return new LiveDataPoint(DateTime.UtcNow, packet);
+				}
+			}	
+		}
+	}
+}
+
+public class LiveDataPoint {
+	public DateTime time;
+	public int signalStrength;
+	public bool fingerOut;
+	public bool droppingSpO2;
+	public bool beep;
+	public int pulseWaveform;
+	public int barGraph;
+	public bool probeError;
+	public bool searching;
+	public int pulseRate;
+	public int bloodSpO2;
+
+
+	public LiveDataPoint(DateTime time, byte[] data) {
+		if (
+			(((data[0] & 0x80) != 0) != true) &&
+			(((data[0] & 0x80) != 0) != false) &&
+			(((data[0] & 0x80) != 0) != false) &&
+			(((data[0] & 0x80) != 0) != false) &&
+			(((data[0] & 0x80) != 0) != false)
+			) throw new Exception("Invalid data packet.");
+
+		this.time = time;
+
+		// 1st byte
+		signalStrength = data[0] & 0x0f;
+		fingerOut = Convert.ToBoolean(data[0] & 0x10);
+		droppingSpO2 = Convert.ToBoolean(data[0] & 0x20);
+		beep = Convert.ToBoolean(data[0] & 0x40);
+
+		// 2nd byte
+		pulseWaveform = data[1];
+
+		// 3rd byte
+		barGraph = data[2] & 0x0f;
+		probeError = Convert.ToBoolean(data[2] & 0x10);
+		searching = Convert.ToBoolean(data[2] & 0x20);
+		pulseRate = (data[2] & 0x40) << 1;
+
+		// 4th byte
+		pulseRate |= data[3] & 0x7f;
+
+		// 5th byte
+		bloodSpO2 = data[4] & 0x7f;
+	}
+}
---
+++ HeartbeatHorror/Assets/Scripts/HBListener.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections;
+using System.Text;
+using System;
+
+using System.IO.Ports;
+
+
+public class HBListener : MonoBehaviour {
+	//listen for heartbeat data
+
+	//public int UDP_LISTEN_PORT = 50000;
+	//private UdpClient client;
+	//private IPEndPoint remoteEP;
+	//private bool messageReceived = true;
+
+	private CMS50Dplus listener;
+
+
+	void Start() {
+		//foreach (string s in SerialPort.GetPortNames()) print(s);
+		//Debug.Log("Starting Client");
+		//remoteEP = new IPEndPoint(IPAddress.Any, UDP_LISTEN_PORT);
+		//client = new UdpClient(remoteEP);
+		listener = new CMS50Dplus("COM4");
+		StartCoroutine(receiveData());
+
+	}
+
+	IEnumerator receiveData() {
+		IEnumerator reader = listener.getLiveData();
+
+		LiveDataPoint point = null;
+		while (reader.MoveNext()) {
+			point = reader.Current as LiveDataPoint;
+			if (point != null) print("fingerOut: " + point.fingerOut + " pulseRate: " + point.pulseRate + " time: " + point.time);
+			yield return new WaitForSeconds(.01f);
+		}
+	}
+
+		/*IEnumerator receiveData() {
+			while (true) {
+				if (messageReceived) {
+					messageReceived = false;
+					print("Waiting for broadcast");
+					client.BeginReceive(new AsyncCallback(_receiveData), null);
+				}
+
+				//byte[] receivedBytes = client.Receive(ref remoteEP);
+
+				yield return new WaitForSeconds(1);
+			}
+		}
+
+		void _receiveData(IAsyncResult result) {
+			byte[] receivedBytes = client.EndReceive(result, ref remoteEP);
+			print("received: " + Encoding.ASCII.GetString(receivedBytes));
+			messageReceived = true;
+		}*/
+	}
