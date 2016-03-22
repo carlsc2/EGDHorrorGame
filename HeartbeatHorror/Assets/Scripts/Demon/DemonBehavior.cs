aca337e124906ae6aafafbd6b767ed0e329f0111
Craig Carlson <cjcarlson20@gmail.com>
2016-03-22 19:27:38 -0400

    flame interpolation works

---
+++ HeartbeatHorror/Assets/Scripts/Demon/DemonBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonBehavior : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(player.position);
+	}
+}
---
+++ HeartbeatHorror/Assets/Scripts/Player/LanternFlameControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanternFlameControl : MonoBehaviour {
+
+	private Transform monster;
+	public float nearDist = 5;
+	public float farDist = 20;
+	public Material candleMat;
+
+	void Start () {
+		monster = GameObject.FindGameObjectWithTag("Monster").transform;
+	}
+	
+	void Update () {
+		float dist = Vector3.Distance(transform.position, monster.position);
+		float val = 1 - ((dist - nearDist) / farDist);
+		val = Mathf.Clamp01(val);
+		candleMat.SetFloat("_Color", val);
+	}
+}
