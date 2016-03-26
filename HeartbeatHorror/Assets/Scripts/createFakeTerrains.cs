a1505eb8686e51b26208d86019ef0a68a7fed2a7
Craig Carlson <cjcarlson20@gmail.com>
2016-03-25 22:24:39 -0400

    removed dummy mansions; no longer necessary because of fog added realtime directional light to make wrapping transitions 100% seamless

---
+++ HeartbeatHorror/Assets/Scripts/createFakeTerrains.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createFakeTerrains : MonoBehaviour {
+
+	public GameObject dummyMansion;
+
+	// Use this for initialization
+	void Start () {
+		TerrainData t = GetComponent<Terrain>().terrainData;
+
+		float offset = t.size.x;// / 2;
+
+		GameObject tmp;
+
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z + offset);
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z - offset);
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z);
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z + offset);
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z - offset);
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z);
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset);
+		tmp = Terrain.CreateTerrainGameObject(t);
+		tmp.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - offset);	
+
+		print(offset);
+		
+	}
+}
