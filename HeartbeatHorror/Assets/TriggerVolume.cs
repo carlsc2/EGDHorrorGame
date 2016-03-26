469f92a950e3f3494a7ae53eb32b2957e48c7d63
unknown <shahp2@SA2411-PC22.win.rpi.edu>
2016-03-25 20:49:39 -0400

    (Parsh) Added Basic Trigger Volume

---
+++ HeartbeatHorror/Assets/TriggerVolume.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerVolume : MonoBehaviour {
+    public AudioClip sound;
+    public AudioSource source;
+
+    
+    
+    BoxCollider col;
+    [Range(0, 10)]
+    public int triggerRate;
+    
+    public bool playSound;
+    public bool eventTrigger;
+
+
+    private bool triggered;
+	// Use this for initialization
+	void Start () {
+        col = GetComponent<BoxCollider>();
+        source = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter( Collider other) {
+        if (playSound)
+        {
+            source.PlayOneShot(sound);
+        }
+
+    }
+
+
+    void TriggerEvent() { 
+    }
+}
