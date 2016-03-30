e55978a8bf80894c02d393a319fb618de81a36da
Craig Carlson <cjcarlson20@gmail.com>
2016-03-30 00:20:11 -0400

    improved terrain efficiency. automated creation of fake terrains

--- HeartbeatHorror/Assets/Scripts/Demon/DemonBehavior.cs
+++ HeartbeatHorror/Assets/Scripts/Demon/DemonBehavior.cs
@@ -8,10 +8,23 @@ public class DemonBehavior : MonoBehaviour {
 
 	public float fieldOfViewAngle = 20;
 	public float sightDistance = 30;
-	public bool playerInSight = false;
-	public bool playerInRange = false;
+	public bool playerInSight = false;//player is in sight to be chased
+	public bool playerInRange = false;//player is in range to be searched for
+
+	private Vector3 wanderpoint;
 
 	public Material eyeMat;
+
+	void OnDrawGizmosSelected() {
+		if (playerInSight) {
+			Gizmos.color = new Color(1, 1, 0, .25f);
+		}
+		else {
+			Gizmos.color = new Color(1, 0, 0, .25f);
+		}
+		
+		Gizmos.DrawSphere(transform.position, sightDistance);
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -21,9 +34,6 @@ public class DemonBehavior : MonoBehaviour {
 
 	bool check_line_of_sight() {
 		//check if player is in line of sight
-		//if (Vector3.Angle(transform.forward,player.position) < LOS_angle) {
-
-		//}
 		Vector3 direction = player.position - transform.position;
 		float dist = direction.magnitude;
 		float angle = Vector3.Angle(direction, transform.forward);
@@ -39,9 +49,6 @@ public class DemonBehavior : MonoBehaviour {
 					if (hit.collider.transform.root == player) {
 						// ... the player is in sight.
 						playerInSight = true;
-
-						// Set the last global sighting is the players current position.
-						//lastPlayerSighting.position = player.transform.position;
 					}
 				}
 			}
@@ -52,11 +59,47 @@ public class DemonBehavior : MonoBehaviour {
 		return true;
 	}
 
-	
+	bool is_navigating() {
+		// Check if we've reached the destination
+		if (!agent.pathPending) {
+			if (agent.remainingDistance <= agent.stoppingDistance) {
+				if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	void RotateTowards(Transform target) {
+		Vector3 direction = (target.position - transform.position).normalized;
+		Quaternion lookRotation = Quaternion.LookRotation(direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * agent.angularSpeed);
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-		//agent.SetDestination(player.position);
 		check_line_of_sight();
+		/*
+		if (playerInSight) {//if player in sight, chase the player
+			RotateTowards(player);
+			agent.SetDestination(player.position);
+		}
+
+		else if (playerInRange && !playerInSight && !is_navigating()) { //if player is near but not seen, wander area
+			wanderpoint = (Random.onUnitSphere * sightDistance) + transform.position;
+			agent.SetDestination(wanderpoint);
+		}
+
+		else if(!playerInRange){//wander larger area
+			wanderpoint = (Random.onUnitSphere * sightDistance * 5) + transform.position;
+			agent.SetDestination(wanderpoint);
+		}*/
+
+
+
+		//eye details
 		Color eyecolor = eyeMat.GetColor("_EmissionColor");
 		Color targetcolor = playerInSight ? Color.red : Color.white;
 		int brightval = playerInRange ? 1 : 0;
