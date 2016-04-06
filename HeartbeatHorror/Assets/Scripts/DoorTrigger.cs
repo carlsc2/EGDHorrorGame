6234d17bd48126ffe10a5009da959ab6caf55d5b
Craig Carlson <cjcarlson20@gmail.com>
2016-04-06 00:07:09 -0400

    ayy lmao

--- HeartbeatHorror/Assets/Scripts/DoorTrigger.cs
+++ HeartbeatHorror/Assets/Scripts/DoorTrigger.cs
@@ -2,36 +2,28 @@
 using System.Collections;
 
 public class DoorTrigger : MonoBehaviour {
-    [Range(1, 10)]
-    public int triggerRate;
-    public Animator anim;
-    
+	[Range(1, 10)]
+	public int triggerRate;
+	public Animator anim;
+	
 	// Use this for initialization
 
-    void Awake() {
-        anim = GetComponent<Animator>();
-    }
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Awake() {
+		anim = GetComponent<Animator>();
 	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        
-            anim.SetTrigger("Door Open");
-        
-        
-        Debug.Log("I've hit something " + other.tag.ToString());
-     }
+	void OnTriggerEnter(Collider other)
+	{
+		
+			anim.SetTrigger("Door Open");
+		
+		
+		Debug.Log("I've hit something " + other.tag.ToString());
+	 }
 
-    void OnTriggerExit(Collider other) {
+	void OnTriggerExit(Collider other) {
 
-        anim.SetTrigger("Door Open");
-        Debug.Log("..and it's gone...");
-    }
+		anim.SetTrigger("Door Open");
+		Debug.Log("..and it's gone...");
+	}
 }
---
+++ HeartbeatHorror/Assets/Shaders/terrainBlend.shader
@@ -0,0 +1,39 @@
+﻿Shader "Custom/WorldSpaceTiling" {
+	Properties{
+		_MainTex("Texture", 2D) = "white" {}
+		_Metallic("Metallic", float) = 1.0
+		_ScaleX("Scale X", float) = 1.0
+		_ScaleY("Scale Y", float) = 1.0
+	}
+	SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		CGPROGRAM
+		#include "UnityPBSLighting.cginc"
+		#pragma surface surf Standard
+		struct Input {
+			float2 uv_MainTex;
+			float3 worldPos;
+			float3 worldNormal; INTERNAL_DATA
+		};
+		sampler2D _MainTex;
+
+		half _ScaleX;
+		half _ScaleY;
+		half _Metallic;
+
+		void surf(Input IN, inout SurfaceOutputStandard o) {
+			float3 correctWorldNormal = WorldNormalVector(IN, float3(0, 0, 1));
+			float2 uv = IN.worldPos.xz;
+
+			if (abs(correctWorldNormal.x) > 0.5) uv = IN.worldPos.yz;
+			if (abs(correctWorldNormal.z) > 0.5) uv = IN.worldPos.xz;
+
+			uv.x /= _ScaleX;
+			uv.y /= _ScaleY;
+			o.Albedo = tex2D(_MainTex, uv).rgb;
+			o.Metallic = _Metallic;
+		}
+		ENDCG
+	}
+	Fallback "Diffuse"
+}
