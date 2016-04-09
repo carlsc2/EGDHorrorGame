19e25d6517a48388a610b42299f391cb2f3e173f
unknown <SHAHP2@SA2411-PC22.win.rpi.edu>
2016-04-08 21:54:44 -0400

    (Parsh) there's a buncha shit here for the inventory system as well as the alter stuff. should be function for Gamefest demo

---
+++ HeartbeatHorror/Assets/Scripts/HardCodedInv.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+/*Skull
+Goblet
+Dagger
+Ring
+*/
+
+ public class HardCodedInv : MonoBehaviour {
+     public enum Artifact
+     {
+         Skull = 0,
+         Goblet = 1,
+         Dagger = 2,
+         Ring = 3
+
+     };
+     public bool HasRing, HasGoblet, HasDagger, HasSkull = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Ring")
+        {
+            HasRing = true;
+            Destroy(other.gameObject);
+
+
+        }
+        if (other.gameObject.tag == "Skull")
+        {
+            HasSkull = true;
+            Destroy(other.gameObject);
+
+        }
+        if (other.gameObject.tag == "Goblet")
+        {
+            HasGoblet = true;
+            Destroy(other.gameObject);
+
+        }
+        if (other.gameObject.tag == "Dagger")
+        {
+            HasDagger = true;
+            Destroy(other.gameObject);
+        } 
+    }
+
+}
