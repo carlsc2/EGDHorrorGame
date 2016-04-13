b40ba1c417e6eb2d7f5fa113bd0631bde7a5ce72
Parshva Shah <g7parsh@gmail.com>
2016-04-13 01:23:00 -0400

    Made The door Slightly Better. Now it can rotate

---
+++ HeartbeatHorror/Assets/OpenFronDoor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenFronDoor : MonoBehaviour {
+   public GameObject LeftControl;
+    public GameObject RightControl;
+
+
+	// Use this for initialization
+	void Start () {
+        LeftControl = GameObject.Find("LeftControl");
+
+        RightControl = GameObject.Find("RightControl");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
