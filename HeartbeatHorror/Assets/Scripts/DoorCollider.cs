1fd96d4d8c066a970a2654ab57119d8494e5d936
Parshva Shah <g7parsh@gmail.com>
2016-04-26 22:12:46 -0400

    Doors and GUI!

---
+++ HeartbeatHorror/Assets/Scripts/DoorCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorCollider : MonoBehaviour {
+
+
+    public bool PlayerHit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other){
+        if (other.tag == "Player")
+        {
+            PlayerHit = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if (other.tag == "Player")
+        {
+            PlayerHit = false;
+        }
+    }
+}
---
+++ HeartbeatHorror/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+public class Menu : MonoBehaviour {
+
+    private Canvas can;
+    private bool active;
+	// Use this for initialization
+	void Start () {
+        can = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Pause"))
+        {
+            active = !active;
+        }
+        if (active)
+        {
+            can.enabled = true;
+        }
+        else
+        {
+            can.enabled = false;
+        }
+
+	}
+}
