a5c8e5b7b831b8e4e35767d29150571ecb462032
Parshva Shah <g7parsh@gmail.com>
2016-04-28 19:48:55 -0400

    Front Door: Now with more "Use" key

--- HeartbeatHorror/Assets/Scripts/OpenFronDoor.cs
+++ HeartbeatHorror/Assets/Scripts/OpenFronDoor.cs
@@ -1,42 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 [RequireComponent( typeof(Animator))]
+[RequireComponent(typeof(DoorCollider))]
 public class OpenFronDoor : MonoBehaviour {
    public GameObject LeftControl;
     public GameObject RightControl;
 
     public Animator anim;
-    
+    public float timer = 3;
+    public bool startTimer = false;
+    DoorCollider dc;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         LeftControl = gameObject.transform.FindChild("LeftControl").gameObject;
 
         RightControl = gameObject.transform.FindChild("RightControl").gameObject;
 
         anim = GetComponent<Animator>();
+        dc = GetComponent<DoorCollider>();
         
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (dc.PlayerHit && Input.GetButtonDown("Use"))
+        {
+            anim.SetBool("DoorOpen", true);
+          
+            startTimer = true;
+
+        }
+        if (startTimer && timer > 0)
+        {
+            timer -= Time.deltaTime;
+            print(timer);
+        }
+        else
+        {
+            anim.SetBool("DoorOpen", false);
+            
+
+        }
+
+        if (!dc.PlayerHit && timer <= 0)
+        {
+            timer = 3;
+            startTimer = false;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            anim.SetBool("DoorOpen", true);
-        }
+       
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && timer <=0 )
         {
-            anim.SetBool("DoorOpen", false);
+            
         }
     }
 }
