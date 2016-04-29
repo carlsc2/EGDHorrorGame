4ae29b706c8c7c2520cc4743b1119bc55287c9fd
Craig Carlson <cjcarlson20@gmail.com>
2016-04-29 15:21:06 -0400

    fixed fps

--- HeartbeatHorror/Assets/Scripts/Player/LanternFlameControl.cs
+++ HeartbeatHorror/Assets/Scripts/Player/LanternFlameControl.cs
@@ -11,30 +11,47 @@ public class LanternFlameControl : MonoBehaviour {
 
 	public float alertdist = 20;
 
+	private float targetgreen;
+
+	public bool do_green_check = false;
+
 
 	void Awake () {
 		candleMat = flameObj.GetComponent<MeshRenderer>().material;
 		candleMat.SetFloat("_Color", 0);
 		candleMat.SetFloat("_Green", 0);
 		monster = GameObject.FindGameObjectWithTag("Monster").transform;
+		if (do_green_check) {
+			StartCoroutine(check_item_dist());
+		}
+	}
+
+	IEnumerator check_item_dist() {
+		while (true) {
+			float itemdist = alertdist;
+			foreach (Collider col in Physics.OverlapSphere(transform.position, alertdist)) {
+				if (col.GetComponent<Item>() != null) {
+					itemdist = Vector3.Distance(transform.position, col.transform.position);
+					break;
+				}
+			}
+			targetgreen = 1 - Mathf.Clamp01(itemdist / alertdist);
+
+			yield return new WaitForSeconds(.5f);
+		}
 	}
 	
 	void Update () {
-		float itemdist = alertdist;
-		foreach(Collider col in  Physics.OverlapSphere(transform.position, alertdist)) {
-			if(col.GetComponent<Item>() != null) {
-				itemdist = Vector3.Distance(transform.position, col.transform.position);
-				break;
-			}
-		}
 
+		float curval = candleMat.GetFloat("_Green");
+		candleMat.SetFloat("_Green", Mathf.MoveTowards(curval,targetgreen,Time.deltaTime));
 
-		if(monster != null) {
+		if (monster != null) {
 			float dist = Vector3.Distance(transform.position, monster.position);
 			float val = 1 - ((dist - nearDist) / (farDist - nearDist));
 			val = Mathf.Clamp01(val);
 			candleMat.SetFloat("_Color", val);
-			candleMat.SetFloat("_Green", 1 - Mathf.Clamp01(itemdist /alertdist));
+			
 		}
 	}
 }
