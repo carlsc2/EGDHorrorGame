020f2f9d1ad2019c71f84333cb337924e8798f7c
Craig Carlson <cjcarlson20@gmail.com>
2016-04-29 22:16:48 -0400

    what did i even do

---
+++ HeartbeatHorror/Assets/Scripts/UIButtonActions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonActions : MonoBehaviour {
+
+	public void Quit() {
+		Application.Quit();
+	}
+
+	public void LoadScene(string scene) {
+		SceneManager.LoadScene(scene);
+	}
+}
