6c039b8135c5e44af168b013b663be233914cac7
Parshva Shah <g7parsh@gmail.com>
2016-04-29 23:11:34 -0400

    I baked Lighting for the final scene. 80 frames on fasted setting. Also fixed the VR thing being off center all the time.

--- HeartbeatHorror/Assets/Scripts/ResetVR.cs
+++ HeartbeatHorror/Assets/Scripts/ResetVR.cs
@@ -6,12 +6,14 @@ public class ResetVR : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         UnityEngine.VR.InputTracking.Recenter();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Reset"))
         {
+            print("RECENTER!");
             UnityEngine.VR.InputTracking.Recenter();
         }
 	}
