9c994f3d0ab0eff983f93ef413fa55192d931a8c
Parshva Shah <g7parsh@gmail.com>
2016-05-04 16:43:20 -0400

    Added a new VR rotation thing for Rob to test.

---
+++ HeartbeatHorror/Assets/VRCorrectCam.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class VRCorrectCam : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (UnityEngine.VR.VRDevice.isPresent) {
+            Vector3 newRot = new Vector3(0,0,0); 
+            newRot.y = transform.rotation.eulerAngles.y;
+            transform.parent.rotation = Quaternion.Euler(newRot);
+            newRot = transform.rotation.eulerAngles;
+            newRot.y = 0;
+            transform.rotation = (Quaternion.Euler(newRot));
+        }
+	}
+}
